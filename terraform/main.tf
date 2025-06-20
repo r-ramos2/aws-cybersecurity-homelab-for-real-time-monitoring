24d16cf8239d89872f4e95671f5a7f5ae43b4496
R <90172092+r-ramos2@users.noreply.github.com>
2025-06-20 14:14:23 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -216,6 +216,8 @@ resource "aws_instance" "kali" {
   vpc_security_group_ids      = [aws_security_group.win_kali_sg.id]
   associate_public_ip_address = true
   key_name                    = aws_key_pair.deployer.key_name
+
+  # Bootstrap via external script
   user_data                   = file("${path.module}/../scripts/kali_setup.sh")
 
   root_block_device {
