84992568be0a298afd71ca4852d2488b227554ef
R <90172092+r-ramos2@users.noreply.github.com>
2025-09-16 20:15:49 -0600

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
-output "deployer_key_path" {
-  description = "Path to the SSH private key generated for this lab"
-  value       = "${path.module}/../deployer_key.pem"
+output "deployer_key_name" {
+  description = "Name of the key pair uploaded to AWS"
+  value       = aws_key_pair.deployer.key_name
 }
 
 output "kali_public_ip" {
@@ -15,5 +15,5 @@ output "windows_public_ip" {
 
 output "tools_public_ip" {
   description = "Public IP of the Security Tools (Ubuntu) instance"
-  value       = aws_instance.security_tools.public_ip
+  value       = aws_instance.tools.public_ip
 }
