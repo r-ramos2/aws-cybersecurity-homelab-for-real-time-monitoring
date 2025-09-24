0614a9ed33b3151705d798186f4998817ff15c31
R <90172092+r-ramos2@users.noreply.github.com>
2025-09-24 17:10:09 -0600

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,9 @@
+output "private_key_path" {
+  description = "Path to the generated SSH private key"
+  value       = local_file.private_key_pem.filename
+  sensitive   = true
+}
+
 output "deployer_key_name" {
   description = "Name of the key pair uploaded to AWS"
   value       = aws_key_pair.deployer.key_name
@@ -16,4 +22,14 @@ output "windows_public_ip" {
 output "tools_public_ip" {
   description = "Public IP of the Security Tools (Ubuntu) instance"
   value       = aws_instance.tools.public_ip
+}
+
+output "splunk_url" {
+  description = "Splunk Web UI URL"
+  value       = "http://${aws_instance.tools.public_ip}:${var.splunk_web_port}"
+}
+
+output "nessus_url" {
+  description = "Nessus Web UI URL"
+  value       = "https://${aws_instance.tools.public_ip}:${var.nessus_port}"
 }
