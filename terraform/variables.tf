93fcf6532fd92bc80729641e0e7133d2917b44de
R <90172092+r-ramos2@users.noreply.github.com>
2026-05-15 20:15:20 -0600

    Eliminate duplicate private_subnet_cidr variable
    Removed duplicate definition of the private_subnet_cidr variable to streamline configuration.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,16 +24,16 @@ variable "public_subnet_cidr" {
   default     = "10.0.1.0/24"
 }
 
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet (Windows victim machine)"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
 variable "availability_zone" {
   description = "AZ for the public subnet"
   type        = string
   default     = "us-east-1a"
-}
-
-variable "private_subnet_cidr" {
-  description = "CIDR block for the private subnet (Windows victim)"
-  type        = string
-  default     = "10.0.2.0/24"
 }
 
 # AMI Lookup
